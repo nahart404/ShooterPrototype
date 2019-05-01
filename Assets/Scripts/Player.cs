6c7c9d3057438818222c8153fbcd89faa0675b6e
nahart <nahart404@gmail.com>
2019-05-01 10:34:57 -0700

    Created Player Movement
    Player movement and limits have been added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,10 +8,27 @@ public class Player : MonoBehaviour
     //variables
     float moveSpeed = 10f;
 
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+    float shipPadding = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetMoveBoundaries();
+    }
+
+    //method to control where the player can and can't go using the camera's viewport
+    private void SetMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main; //main camera in the scene
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + shipPadding; //min of x axis
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - shipPadding; //max of x axis 
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + shipPadding; //min of y axis
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - shipPadding; //max of y axis 
     }
 
     // Update is called once per frame
@@ -23,15 +41,14 @@ public class Player : MonoBehaviour
     private void Move()
     {
         //horizonal movement
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed; 
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         //Time.deltaTime makes it to where movement 
         //doesn't depend on a player's framerate (faster fro faster framerates, slower for less frames)
-
-        var newXPos = transform.position.x + deltaX; //player's soon to be new x position + delta(change)
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax); //player's soon to be new x position + delta(change) with limits
 
         //vertical movement
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = transform.position.y + deltaY;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos); //set new position
 
