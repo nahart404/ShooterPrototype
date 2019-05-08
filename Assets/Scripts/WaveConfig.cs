582e39ceb4f679f6c18ff9fa07ed1c83b942ca68
nahart <nahart404@gmail.com>
2019-05-08 12:47:25 -0700

    Created WaveConfig
    WaveConfig is a scriptable object used to hold and pass on info about the enemy waves for other classes

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Wave Config")]
+public class WaveConfig : ScriptableObject
+{
+    //varaibles
+
+    //config
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawn = .5f;
+    [SerializeField] float spawnRandomizer = .3f;
+    [SerializeField] int numOfEnemies = 5;
+    [SerializeField] float moveSpeed = 2f;
+
+    public GameObject getEnemyPrefab()
+    {   return enemyPrefab;    }
+
+    public GameObject getPathPrefab()
+    {   return pathPrefab;    }
+
+    public float getTimeBetweenSpawn()
+    {   return timeBetweenSpawn;   }
+
+    public int getNumOfEnemies()
+    { return numOfEnemies; }
+
+    public float getMoveSpeed()
+    { return moveSpeed; }
+
+    public float getSpawnRandomizer()
+    { return spawnRandomizer; }
+}
