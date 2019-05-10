f5fbe0498a46e6c923b082521b7e2542e6efa959
nahart <nahart404@gmail.com>
2019-05-10 14:37:42 -0700

    Enemy Health and Damage
    Enemies now have health and can be dealt damage by the player now

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100; //amount of damage
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    public void Hit()
+    {
+        //when hit, destroy gameObject
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //thing that bummed into enemy
+    {
+        //set dmgDealer to equal the laser (aka the "other")
+        DamageDealer dmgDealer = other.gameObject.GetComponent<DamageDealer>();
+        DealDamage(dmgDealer);
+    }
+
+    private void DealDamage(DamageDealer dmgDealer)
+    {
+        health -= dmgDealer.GetDamage(); //when hit,take away the health of the enemy
+
+        //if 0 health,
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
